11b3005d8558e752c728577430cf54cd9e0d602c
Nicolas Cervantes <50224828+NicolasCervantes@users.noreply.github.com>
2019-08-22 18:08:31 -0500

    Update Punto1.R

--- Talleres/TallerMatrices/Punto1.R
+++ Talleres/TallerMatrices/Punto1.R
@@ -1,85 +1,76 @@
-#Punto 1
-library(pracma)
-library(Matrix)
-
-#Parte a
-n=3
-D1<-eye(n, m=n)#Diagonal de la matriz 
-D2<-ones(n, m=n)#Llenar matriz de 1
-D3<-zeros(n, m=n)#Llenar matriz de 0
-
-#Matriz A
-A = matrix(c(8, 9, 2,
-             2, 7, 2,
-             2, 8, 6), 
-           nrow=n, byrow=TRUE)
-
-#Matriz B
-b = matrix(c(69,47,68), nrow=n, byrow=TRUE)
-
-print(D1)
-print(D2)
-print(D3)
-
-#Parte b
-# w grado de relajación 0<w<2
-fSOR<-function(a,n,w){
-  D=a*eye(n, m=n)
-  L<-a
-  L[lower.tri(L, diag = FALSE)]<-0
-  auxT=(D-(w*L))
-  auxT1=inv(auxT)
-  U<-a
-  U[upper.tri(U, diag = FALSE)]<-0
-  auxT2=((1-w)*D+(w*U))
-  Tfinal=auxT1*auxT2
-}
-print(fSOR(A,3,1.5))
-
-----------------------------------------------------------------------
-
-#Segunda Matriz 
-
-library(pracma)
-library(Matrix)
-
-#Parte a
-n=6
-c=6
-D1<-eye(n, m=n)#Diagonal de la matriz 
-D2<-ones(n, m=n)#Llenar matriz de 1
-D3<-zeros(n, m=n)#Llenar matriz de 0
-
-#Matriz A
-A = matrix(c(sample(0:20), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), 
-             sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1),
-             sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1),
-             sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1),
-             sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1),
-             sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1), sample(0:20,1)),
-             nrow=n, ncol=c, byrow=TRUE)
-
+#Punto 1 
+n = 6
+rrr =sample(0:20, 36, replace = TRUE)
+A = matrix( rrr, nrow=6, byrow=TRUE)
 print(A)
 
-#Matriz B
-b = matrix(c(1,2,3,4,5,6), nrow=n, byrow=TRUE)
-
-print(D1)
-print(D2)
-print(D3)
-
-#Parte b
-# w grado de relajación 0<w<2
-fSOR<-function(a,n,w){
-  D=a*eye(n, m=n)
-  L<-a
-  L[lower.tri(L, diag = FALSE)]<-0
-  auxT=(D-(w*L))
-  auxT1=inv(auxT)
-  U<-a
-  U[upper.tri(U, diag = FALSE)]<-0
-  auxT2=((1-w)*D+(w*U))
-  Tfinal=auxT1*auxT2
+cond = 0
+while(cond< 1000 )
+{
+  A = matrix( sample(10:20, 36, replace = TRUE), nrow=n, byrow=TRUE)
+  cond = condicional(A)
 }
-print(fSOR(A,6,1.5))
+print("A")
+print(A)
 
+cond = (condicional(A))
+print(cond)
+b = matrix( c(1,2,3,4,5,6), nrow = 1, byrow=TRUE)
+print("b")
+print(b)
+
+if(cond > 1000)
+{
+  print(cond)  
+  diagonal <- function(M) 
+  {
+    M[col(M)!=row(M)] <- 0
+    return(M)
+  }
+  
+  
+  #T = -D^-1(L + U)
+  D = diagonal(A)
+  L = tril(A,k=-1)
+  U = triu(A,k=1)
+  
+  T = (-solve(D))%*%(L+U)
+  print("Matriz de transición")
+  print(T)
+  print("Norma")
+  norma <- norm(T,"F")
+  print(norma)
+  print("Radio espectral de la matriz")
+  
+  ev <- eig(A)
+  abev <- abs(ev)
+  radioExp <- max(abev)
+  print( radioExp)
+  
+  
+  # Matriz diagonal de dimension 3
+  I=diag(1,nrow = nrow(A))
+  # Matriz inversa de A
+  D1 <- solve(D,I)
+  T1 = D1 %*% U
+  T2 = (I + (L %*% D1))
+  # Matriz inversa de A
+  T2<- solve(T2,I)
+  
+  #Analisi de convergencia
+  MatTG = T1+T2
+  normaG = norm(MatTG, type = c( "I"))
+  print("Norma/convergencia de Gauss")
+  print(normaG)
+  print("Matriz de transición Gauss ")
+  print(MatTG)
+  
+  MatTJ = (-D1)%*%(L+U)
+  normaJ = norm(MatTJ, type = c("I"))
+  print("Norma/convergencia de Jacobi")
+  print(normaJ)
+  print("Matriz de transición Jacobi")
+  print(MatTJ)
+  
+  
+}
