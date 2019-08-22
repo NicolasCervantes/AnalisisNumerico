b79a7f246a8939b7f7f2aba3790e8e112f93e459
Nicolas Cervantes <50224828+NicolasCervantes@users.noreply.github.com>
2019-08-22 18:18:55 -0500

    Update Punto1.R

--- Talleres/TallerMatrices/Punto1.R
+++ Talleres/TallerMatrices/Punto1.R
@@ -1,4 +1,4 @@
-#Punto 1 
+#Punto 1 Adicional Clase  
 n = 6
 rrr =sample(0:20, 36, replace = TRUE)
 A = matrix( rrr, nrow=6, byrow=TRUE)
@@ -57,7 +57,7 @@ if(cond > 1000)
   # Matriz inversa de A
   T2<- solve(T2,I)
   
-  #Analisi de convergencia
+  #Análisis de convergencia
   MatTG = T1+T2
   normaG = norm(MatTG, type = c( "I"))
   print("Norma/convergencia de Gauss")
@@ -74,3 +74,64 @@ if(cond > 1000)
   
   
 }
+
+
+
+# Punto 1 Taller. 
+#Para el siguiente ejercicio, instale el paquete "pracma"
+
+#-- a --
+#Revise las siguientes funciones con la matriz del ejercicio 2
+
+
+#Tamaño de la matriz
+n = 4
+A = matrix(c(2, 7, -6, 9,
+             5, 1,8, 12,
+             0, 6, 5, 8,
+             3, 7, 1, 2), nrow=n, byrow=TRUE)
+print("A")
+print(A)
+
+b = matrix(c(1.5,3,5,-4), nrow=n, byrow=TRUE)
+print("b")
+print(b)
+
+
+D1 <- eye(n, m = n)
+D2 <- ones(n, m = n)
+D3 <- zeros(n, m = n)
+
+print("D1")
+D1
+print("D2")
+D2
+print("D3")
+D3
+
+#El comando eye(n, m = n) permite obtener la matriz identidad de tamaño n, la cual está compuesta de ceros a excepción de la diagonal principal que contiene unos.
+#El comando ones(n, m = n) permite obtener una matriz de tamaño n contenida únicamnete por unos.
+#El comando zeros(n, m = n) permite obtener una matriz de tamaño n compuesta de ceros.
+
+
+
+#-- b --
+# b. Evalue la matriz de transicionn para el metodo SOR
+
+U = A
+L = A
+
+#Aca se asignan las respectivas matrices diagonalizadas a cada variable
+U[upper.tri(U, diag = TRUE)] <- 0
+L[lower.tri(L,diag=TRUE)] <- 0
+print (A)
+
+#Obtienee la diagonal principal de la matriz
+D1 = diag(diag(A^-1))
+
+#Multiplicación de la matriz
+T1 = D1 %*% U
+I = D1
+T2 = (I + (L %*% D1))
+MatT = T1+T2
+print(MatT)
