703344cdeac2b9e49cbe802749ed1eb8c897520b
Nicolas Cervantes <50224828+NicolasCervantes@users.noreply.github.com>
2019-08-25 17:54:52 -0500

    Create Punto1b.py

---
+++ Parciales/Parcial_1/Punto1b.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+def sumaElementos(x, n):
+  suma=0
+  data=[]
+  for i in range(n):
+    for j in range(n):
+      suma+=x[i][j]
+      data.append(suma)
+  plt.plot(data)
+  plt.ylabel('Convergencia')
+  plt.show()
+
+A=[[3,4,5],[1,9,12],[1,9,2]]
+sumaElementos(A,3)
